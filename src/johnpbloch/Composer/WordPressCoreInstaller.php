f0442aeadefe2f5d5759ff6f3dcc8e33365a4ec3
Joost Berculo <jberculo@gmail.com>
2018-06-22 20:53:06 +0200

    remove dot from sensitive dirs

--- src/johnpbloch/Composer/WordPressCoreInstaller.php
+++ src/johnpbloch/Composer/WordPressCoreInstaller.php
@@ -25,87 +25,92 @@ use Composer\Config;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 
-class WordPressCoreInstaller extends LibraryInstaller {
+class WordPressCoreInstaller extends LibraryInstaller
+{
 
-	const TYPE = 'wordpress-core';
+    const TYPE = 'wordpress-core';
 
-	const MESSAGE_CONFLICT = 'Two packages (%s and %s) cannot share the same directory!';
-	const MESSAGE_SENSITIVE = 'Warning! %s is an invalid WordPress install directory (from %s)!';
+    const MESSAGE_CONFLICT = 'Two packages (%s and %s) cannot share the same directory!';
+    const MESSAGE_SENSITIVE = 'Warning! %s is an invalid WordPress install directory (from %s)!';
 
-	private static $_installedPaths = array();
+    private static $_installedPaths = [];
 
-	private $sensitiveDirectories = array( '.' );
+    private $sensitiveDirectories = [];
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function getInstallPath( PackageInterface $package ) {
-		$installationDir = false;
-		$prettyName      = $package->getPrettyName();
-		if ( $this->composer->getPackage() ) {
-			$topExtra = $this->composer->getPackage()->getExtra();
-			if ( isset( $topExtra['wordpress-install-dir'] ) ) {
-				$installationDir = $topExtra['wordpress-install-dir'];
-				if ( is_array( $installationDir ) ) {
-					$installationDir = empty( $installationDir[ $prettyName ] ) ? false : $installationDir[ $prettyName ];
-				}
-			}
-		}
-		$extra = $package->getExtra();
-		if ( $installationDir === false && isset( $extra['wordpress-install-dir'] ) ) {
-			$installationDir = $extra['wordpress-install-dir'];
-		} elseif ($installationDir === false ) {
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        $installationDir = false;
+        $prettyName      = $package->getPrettyName();
+        if ($this->composer->getPackage()) {
+            $topExtra = $this->composer->getPackage()->getExtra();
+            if (isset($topExtra['wordpress-install-dir'])) {
+                $installationDir = $topExtra['wordpress-install-dir'];
+                if (is_array($installationDir)) {
+                    $installationDir = empty($installationDir[$prettyName]) ? false : $installationDir[$prettyName];
+                }
+            }
+        }
+        $extra = $package->getExtra();
+        if ($installationDir === false && isset($extra['wordpress-install-dir'])) {
+            $installationDir = $extra['wordpress-install-dir'];
+        } elseif ($installationDir === false) {
             $installationDir = 'wordpress';
         }
-		$vendorDir = $this->composer->getConfig()->get( 'vendor-dir', Config::RELATIVE_PATHS ) ?: 'vendor';
-		if (
-			in_array( $installationDir, $this->sensitiveDirectories ) ||
-			( $installationDir === $vendorDir )
-		) {
-			throw new \InvalidArgumentException( $this->getSensitiveDirectoryMessage( $installationDir, $prettyName ) );
-		}
-		if (
-			! empty( self::$_installedPaths[ $installationDir ] ) &&
-			$prettyName !== self::$_installedPaths[ $installationDir ]
-		) {
-			$conflict_message = $this->getConflictMessage( $prettyName, self::$_installedPaths[ $installationDir ] );
-			throw new \InvalidArgumentException( $conflict_message );
-		}
-		self::$_installedPaths[ $installationDir ] = $prettyName;
+        $vendorDir = $this->composer->getConfig()->get('vendor-dir', Config::RELATIVE_PATHS) ?: 'vendor';
+        if (
+            in_array($installationDir, $this->sensitiveDirectories) ||
+            ($installationDir === $vendorDir)
+        ) {
+            throw new \InvalidArgumentException($this->getSensitiveDirectoryMessage($installationDir, $prettyName));
+        }
+        if (
+            ! empty(self::$_installedPaths[$installationDir]) &&
+            $prettyName !== self::$_installedPaths[$installationDir]
+        ) {
+            $conflict_message = $this->getConflictMessage($prettyName, self::$_installedPaths[$installationDir]);
+            throw new \InvalidArgumentException($conflict_message);
+        }
+        self::$_installedPaths[$installationDir] = $prettyName;
 
-        echo $installationDir."<<<<<<<<";
+        echo $installationDir . "<<<<<<<<";
 		return $installationDir;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public function supports( $packageType ) {
-		return self::TYPE === $packageType;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return self::TYPE === $packageType;
+    }
 
-	/**
-	 * Get the exception message with conflicting packages
-	 *
-	 * @param string $attempted
-	 * @param string $alreadyExists
-	 *
-	 * @return string
-	 */
-	private function getConflictMessage( $attempted, $alreadyExists ) {
-		return sprintf( self::MESSAGE_CONFLICT, $attempted, $alreadyExists );
-	}
+    /**
+     * Get the exception message with conflicting packages
+     *
+     * @param string $attempted
+     * @param string $alreadyExists
+     *
+     * @return string
+     */
+    private function getConflictMessage($attempted, $alreadyExists)
+    {
+        return sprintf(self::MESSAGE_CONFLICT, $attempted, $alreadyExists);
+    }
 
-	/**
-	 * Get the exception message for attempted sensitive directories
-	 *
-	 * @param string $attempted
-	 * @param string $packageName
-	 *
-	 * @return string
-	 */
-	private function getSensitiveDirectoryMessage( $attempted, $packageName ) {
-		return sprintf( self::MESSAGE_SENSITIVE, $attempted, $packageName );
-	}
+    /**
+     * Get the exception message for attempted sensitive directories
+     *
+     * @param string $attempted
+     * @param string $packageName
+     *
+     * @return string
+     */
+    private function getSensitiveDirectoryMessage($attempted, $packageName)
+    {
+        return sprintf(self::MESSAGE_SENSITIVE, $attempted, $packageName);
+    }
 
 }
